11497f24e01bbf6d211ce601f55f74e79000d2e6
Esoteric918 <3411@holbertonschool.com>
2022-08-22 12:57:39 -0500

    fix merge conflict

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // DBClient should have:
 
 // the constructor that creates a client to MongoDB:
@@ -8,10 +9,13 @@
 // an asynchronous function nbUsers that returns the number of documents in the collection users
 // an asynchronous function nbFiles that returns the number of documents in the collection files
 // After the class definition, create and export an instance of DBClient called dbClient
+=======
+>>>>>>> Still working on redis.js
 const { MongoClient } = require('mongodb');
 
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
+<<<<<<< HEAD
 const database = process.env.DB_DATABASE || 'files_manager';
 
 const url = `mongodb://${host}:${port}/${database}`;
@@ -27,11 +31,24 @@ class DBClient {
       if (err) {
         console.log(err);
         this.db = false;
+=======
+const dbFile = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+  constructor() {
+    MongoClient.connect(url, (err, client) => {
+      if (err) {
+        this.db = false;
+      } else {
+        this.db = client.db(dbFile);
+>>>>>>> Still working on redis.js
       }
     });
   }
 
   isAlive() {
+<<<<<<< HEAD
     if (!this.db) {
       return !!this.db;
     }
@@ -46,6 +63,20 @@ class DBClient {
   async nbFiles() {
     const numOfFiles = await this.files.countDocuments({});
     return numOfFiles;
+=======
+    if (this.db) return true;
+    return false;
+  }
+
+  async nbUsers() {
+    const users = await this.db.collection('users').countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    const files = await this.db.collection('files').countDocuments();
+    return files;
+>>>>>>> Still working on redis.js
   }
 }
 
--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const redis = require('redis');
 const { promisify } = require('util');
 
@@ -27,8 +28,44 @@ class RedisClient {
   async del(client, key) {
     const result = await promisify(this.client.del).bind(this.client);
     return result(key);
+=======
+const redis = require('redis'); 
+const { promisify } = require('util');
+
+//RedisClient Class to performing operations  
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(error);
+  );
+
+// isAlive() 
+  isAlive() {
+    this.client.connected; 
+
+  async get(key) {
+    // const getValue = await this.getAsync(key);
+    // return value;
+    const getValue = await promisify(this.client.get).bind(this.client);
+    const newKey = await getValue(key);
+    return newKey;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    this.client.delete(key, value);
+>>>>>>> Still working on redis.js
   }
 }
 
 const redisClient = new RedisClient();
+<<<<<<< HEAD
 export default redisClient;
+=======
+
+export default redisClient;
+>>>>>>> Still working on redis.js
