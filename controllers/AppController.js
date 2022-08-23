3fa2fd6dabafc787b69ad7938a7b18bb91c00c5f
Esoteric918 <3411@holbertonschool.com>
2022-08-22 22:22:19 -0500

    update for task 3 still not working 100%

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -10,14 +10,14 @@ class AppController {
     if (Redis.isAlive() && DB.isAlive()) {
       res.status(200).json({ "redis": true, "db": true });
     }
-    return res.status(500).json({ "redis": false, "db": false });
+    return res.status(400).send("Redis or DB is not alive");
   }
   // GET /stats should return the number of users and the number of articles in the DB by using the 2 utils created previously: { "users": 1, "articles": 1 } with a status code 200
   static getStats(req, res) {
     (async () => {
       const users = await DB.nbUsers();
       const files = await DB.nbFiles();
-      res.status(200).json({ "users": users, "files": files });
+      res.status(200).json({ users, files });
     })()
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@
 const express = require('express');
 
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router(); // set up a new router
 
 router.get('/status', AppController.getStatus); // get status of the app
 router.get('/stats', AppController.getStats); // get stats of the app
+
+router.post('/users', UsersController.postNew); // post a new user
 
 module.exports = router; // export router to be used in server.js
