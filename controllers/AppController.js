7e259bdd078ca0e4c5712035e1632819147dd395
RockoGilbert <evolve2122@gmail.com>
2022-08-23 11:39:01 -0500

    Added a controller

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+App Controller 
+import RedisClient from '../utils/redis';
+import DBClient from '../utils/db';
+
+class AppController {
+  static getStatus(response) {
+    const data = {
+      redis: RedisClient.isAlive(),
+      db: DBClient.isAlive(),
+    };
+    return response.status(200).send(status);
+  }
+
+  static async getStats(response) {
+    const stats = {
+      users: await dbClient.users(),
+      files: await dbClient.files(),
+    };
+    return response.status(200).send(stats);
+  }
+}
+
+module.exports = AppController;
